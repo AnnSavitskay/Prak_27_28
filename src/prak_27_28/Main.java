aa19f3fcf20550d06ed5bdb4e9abb77b22c08306
Ann_Savitskay <ansavitskayloki@gmail.com>
2021-10-18 20:06:28 +0300

    Prak_27_28

--- src/prak_27_28/Main.java
+++ src/prak_27_28/Main.java
@@ -1,18 +1,15 @@
 package prak_27_28;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
 import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args)
     {
-        int n = 6;
         Runtime runtime = Runtime.getRuntime();
         int a = runtime.availableProcessors();
-        System.out.println(a);
 
         String srcFolder = "/home/pasta/Pictures/java_start_pictures/";
         String dstFolder = "/home/pasta/Pictures/java_finish_pictures/";
@@ -21,6 +18,8 @@ public class Main {
         long start = System.currentTimeMillis();
 
         File[] files = srcDir.listFiles();
+        int n = files.length/a;
+        ArrayList<File[]> array = new ArrayList<File[]>();
 
         try
         {
@@ -29,53 +28,27 @@ public class Main {
                 Files.createDirectories(Paths.get(dstFolder));
             }
 
-            File[] files1 = new File[n];
-            File[] files2 = new File[n];
-            File[] files3 = new File[n];
-            File[] files4 = new File[n];
-            int i = 0, j = 1;
+            int i = 0;
+            File[] f = new File[n];
             for(File file : files)
             {
-                if((i < 6)&&(j == 4))
+                if(i < n)
                 {
-                    files4[i] = file;
+                    f[i] = file;
                     i++;
                 }
-                if((i < 6)&&(j == 3))
-                {
-                    files3[i] = file;
-                    i++;
-                }
-                if((i < 6)&&(j == 2))
-                {
-                    files2[i] = file;
-                    i++;
-                }
-                if((i < 6)&&(j == 1))
-                {
-                    files1[i] = file;
-                    i++;
-                }
-                if(i >= 6){
-                    j++;
+                if(i >= n){
+                    array.add(f);
                     i = 0;
+                    f = new File[n];
                 }
             }
-            Foto foto1 = new Foto(files1);
-            Foto foto2 = new Foto(files2);
-            Foto foto3 = new Foto(files3);
-            Foto foto4 = new Foto(files4);
-
-
-            Thread thread1 = new Thread(new Minimum(foto1, dstFolder));
-            Thread thread2 = new Thread(new Minimum(foto2, dstFolder));
-            Thread thread3 = new Thread(new Minimum(foto3, dstFolder));
-            Thread thread4 = new Thread(new Minimum(foto4, dstFolder));
-
-            thread1.start();
-            thread2.start();
-            thread3.start();
-            thread4.start();
+            for(int k = 0; k < a; k++)
+            {
+                Foto foto = new Foto(array.get(k));
+                Thread thread = new Thread(new Minimum(foto, dstFolder));
+                thread.start();
+            }
         }
         catch (Exception ex) {
             ex.printStackTrace();
